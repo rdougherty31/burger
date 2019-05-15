5d5ac8637fd6531659c955f61731815c16e07ed3
Rachael <rdougherty031@gmail.com>
2019-05-15 14:47:15 -0400

    comments

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)} else {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,7 +14,7 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -22,5 +23,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
